31a196f81919d6dfd003e3068563f52028db4d4e
wjthompso <49210425+wjthompso@users.noreply.github.com>
2023-03-29 12:09:43 -0700

    Styling icons

--- src/components/searchbar/subcomponents/CategoriesCarousel.vue
+++ src/components/searchbar/subcomponents/CategoriesCarousel.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="carousel-window">
         <div class="carousel-track">
-            <div class="previous-button-fade">
+            <!-- <div class="previous-button-fade">
                 <button class="previous-carousel-btn">
                     <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 12px; width: 12px; stroke: currentcolor; stroke-width: 5.33333; overflow: visible;"><g fill="none"><path d="m20 28-11.29289322-11.2928932c-.39052429-.3905243-.39052429-1.0236893 0-1.4142136l11.29289322-11.2928932"></path></g></svg>
                 </button>
-            </div>
+            </div> -->
             <button v-for="category in categoriesData" class="radio-icon" :key="category.id">
                 <img class="filter-image" :src="category.image" alt="">
                 <p>{{category.name}}</p>
@@ -100,6 +100,16 @@ export default {
                     name: "Mansion",
                     image: require("@/assets/"+"mansions.jpg")
                 },
+                {
+                    id: 16,
+                    name: "Play",
+                    image: require("@/assets/"+"bowling-ball.jpg")
+                },
+                {
+                    id: 17,
+                    name: "Castle",
+                    image: require("@/assets/"+"castle.jpg")
+                },
             ]
         }
         }
@@ -139,9 +149,9 @@ button.radio-icon {
     background-color: transparent;
     align-items: center;
     justify-content: center;
-    /* color: rgb(106, 106, 106); */
-    color: rgb(0, 0, 0);
-    /* opacity: 0.5; */
+    color: rgb(106, 106, 106);
+    /* color: rgb(0, 0, 0); */
+    opacity: 0.5;
     transition: opacity 0.2s ease-in-out;
     margin: 0rem 1rem;
     cursor: pointer;
