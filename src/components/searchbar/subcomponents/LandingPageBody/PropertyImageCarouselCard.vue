1f91016d02184170928bce0cbf99e5a025e2ef94
wjthompso <49210425+wjthompso@users.noreply.github.com>
2023-04-07 15:17:07 -0700

    Handling for multiple prev/next scrolls for rapid button clicks, consistent nav indicator sizing and positioning

--- src/components/searchbar/subcomponents/LandingPageBody/PropertyImageCarouselCard.vue
+++ src/components/searchbar/subcomponents/LandingPageBody/PropertyImageCarouselCard.vue
@@ -1,6 +1,6 @@
 <template>
       <div class="image-card image-card-width">
-        <div class="image-carousel-container">
+        <div class="image-carousel-container" ref="imageCarouselContainer">
             <button class="listing-card-save-button" type="button">
                 <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 30px; width: 30px; overflow: visible;">
                     <path d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"></path></svg>
@@ -8,14 +8,16 @@
             <button class="carousel-button" id="prevButton" @click="previous">
                 <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 12px; width: 12px; stroke: currentcolor; stroke-width: 4; overflow: visible;"><g fill="none"><path d="m20 28-11.29289322-11.2928932c-.39052429-.3905243-.39052429-1.0236893 0-1.4142136l11.29289322-11.2928932"></path></g></svg>
             </button>
-            <div class="carousel-images">
+            <div class="carousel-images" ref="carouselImages">
                 <img v-for="image in imageLocationArray" :src="image.imageLocation" :alt="image.id" :key="image.id" @click="navigateToProperty">
             </div>
             <button class="carousel-button" id="nextButton" @click="next"><svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 12px; width: 12px; stroke: currentcolor; stroke-width: 4; overflow: visible;"><g fill="none"><path d="m12 4 11.2928932 11.2928932c.3905243.3905243.3905243 1.0236893 0 1.4142136l-11.2928932 11.2928932"></path></g></svg></button>
-            <div class="nav-indicator-carousel-window">
-                <div class="nav-indicator-carousel-track">
-                    <!-- Number of nav-indicator-carousel-items must match number of carousel-images -->
-                    <div v-for="image in imageLocationArray" class="nav-indicator-carousel-item" :id="image.id" :key="image.id"></div>
+            <div class="wrapper-for-nav-indicator-carousel">
+                <div class="nav-indicator-carousel-window">
+                    <div class="nav-indicator-carousel-track">
+                        <!-- Number of nav-indicator-carousel-items must match number of carousel-images -->
+                        <div v-for="image in imageLocationArray" class="nav-indicator-carousel-item" :id="image.id" :key="image.id"></div>
+                    </div>
                 </div>
             </div>
 
@@ -49,32 +51,49 @@ export default {
     data() {
         return {
             currentImageIndex: 0,
+            scrollQueue: [],
         }
     },
     mounted() {
-            const carouselImagesSets = document.querySelectorAll('.image-carousel-container');
-            for (let i = 0; i < carouselImagesSets.length; i++) {
-                const imageCount = carouselImagesSets[i].children.length;
-                console.log(imageCount);
+            const carouselImagesSets = this.$refs.imageCarouselContainer;
 
-                // Set the width of the carousel track to number of * width of the .nav-indicator-carousel-item
-                const navCarouselTrack = carouselImagesSets[i].querySelector('.nav-indicator-carousel-track');
-                const navCarouselItems = carouselImagesSets[i].querySelectorAll('.nav-indicator-carousel-item');
-                const navCarouselItemCount = navCarouselTrack.children.length;
+            // Set the width of the carousel track to number of * width of the .nav-indicator-carousel-item
+            const navCarouselTrack = carouselImagesSets.querySelector('.nav-indicator-carousel-track');
+            const navCarouselItems = carouselImagesSets.querySelectorAll('.nav-indicator-carousel-item');
+            const navCarouselItemCount = navCarouselTrack.children.length;
 
-                // Get the width of the carousel item, including margins
-                const carouselItemWidth = this.getFullWidth(navCarouselItems[1]);
+            // Get the width of the carousel item, including margins
+            const carouselItemWidth = this.getFullWidth(navCarouselItems[1]);
 
-                // Set the width of the carousel track to the number of items times the width of a carousel item, including margins
-                navCarouselTrack.style.width = `${navCarouselItemCount * carouselItemWidth}px`;
+            // Set the width of the carousel track to the number of items times the width of a carousel item, including margins
+            navCarouselTrack.style.width = `${navCarouselItemCount * carouselItemWidth}px`;
 
-                // Set the initial nav indicator to active
-                navCarouselItems[0].classList.add('active');
-                navCarouselItems[3].classList.add("near-edge");
-                navCarouselItems[4].classList.add("edge");
-            }
+            // Set the initial nav indicator to active
+            navCarouselItems[0].classList.add('active');
+            navCarouselItems[3].classList.add("near-edge");
+            navCarouselItems[4].classList.add("edge");
+
+            // Add event listeners for window resize events
+            window.addEventListener('resize', this.onWindowResize);
+    },
+    beforeUnmount() {
+        // Remove event listeners for window resize events
+        window.removeEventListener('resize', this.onWindowResize);
     },
     methods: {
+        onWindowResize() {
+            // When the window is resized, re-calculate the width of the carousel track
+            const carouselImages = this.$refs.carouselImages;
+            const firstImage = carouselImages.querySelector('img');
+
+            const imageCarouselItemWidth = firstImage.offsetWidth;
+
+            // Calculate the nearest aligned position based on the updated dimensions
+            const alignedPosition = Math.round(carouselImages.scrollLeft / imageCarouselItemWidth) * imageCarouselItemWidth;
+
+            // Update the scroll position to the nearest aligned position
+            carouselImages.scrollLeft = alignedPosition;
+        },
         getFullWidth(element) {
             // Including the margins
             // offsetWidth includes border, padding, and content
@@ -88,7 +107,66 @@ export default {
         navigateToProperty() {
             this.$router.push('/property/' + this.id);
         },
+        // Preparing to refactor for asynchronous calls to handle multiple button calls
+        async handleButtonClick(e, nextOrPrevFunction) {
+            // Add a new async function to the scrollQueue
+            this.scrollQueue.push(async () => {
+                await nextOrPrevFunction(e);
+            });
+
+            // If there's only one item in the queue (the current one), start processing the queue
+            if (this.scrollQueue.length === 1) {
+                await this.processScrollQueue();
+            }
+            },
+
+            async processScrollQueue() {
+            while (this.scrollQueue.length) {
+                await this.scrollQueue[0](); // Execute the first function in the queue
+                this.scrollQueue.shift(); // Remove the first item from the queue
+            }
+        },
         next(e) {
+            this.handleButtonClick(e, this.performNextScroll);
+        },
+        async smoothScrollBy(element, { left }) {
+            return new Promise((resolve) => {
+                // Set the scroll-behavior property to 'smooth'
+                element.style.scrollBehavior = 'smooth';
+
+                // Perform the scroll
+                element.scrollBy({
+                    left: left,
+                    behavior: 'smooth',
+                });
+
+                // Wait for the 'scroll' event to fire, indicating that the scrolling has started
+                const onScrollStart = () => {
+                    element.removeEventListener('scroll', onScrollStart);
+
+                    // Wait for the 'scroll' event to stop firing for a certain time, indicating that the scrolling has finished
+                    // the .scrollBy method may take more than 100 ms to finish scrolling, but things look good for now so we'll set it
+                    // to that for now
+                    let scrollTimeout;
+                    const onScrollEnd = () => {
+                        clearTimeout(scrollTimeout);
+                        scrollTimeout = setTimeout(() => {
+                            element.removeEventListener('scroll', onScrollEnd);
+
+                            // Set the scroll-behavior property back to 'auto'
+                            element.style.scrollBehavior = 'auto';
+
+                            resolve();
+                            }, 100);
+                    };
+
+                    element.addEventListener('scroll', onScrollEnd);
+                };
+
+                element.addEventListener('scroll', onScrollStart);
+            });
+        },
+        async performNextScroll(e) {
             let imageCarouselContainer;
             imageCarouselContainer = e.target.closest('.image-carousel-container');
             const carouselImages = imageCarouselContainer.querySelector('.carousel-images');
@@ -103,11 +181,6 @@ export default {
             const navCarouselItems = imageCarouselContainer.querySelectorAll('.nav-indicator-carousel-item');
             const carouselItemWidth = this.getFullWidth(navCarouselItems[1]);
 
-            // This is the logic for the image carousel
-            // if (this.currentImageIndex === imageCount-1) {
-            //     return;
-            // }
-            // this.currentImageIndex++;
             carouselImages.style.transition = 'transform 0.4s ease-in-out';
 
             // We want to stop incrementing forward through the images if we are at the last image
@@ -117,24 +190,6 @@ export default {
                 return
             } else {
                 this.currentImageIndex++;
-            }
-
-            console.log(carouselImages.scrollLeft);
-            console.log(imageCount);
-
-            // If the user has scrolled so that we see two images, snap to the next image
-            if (carouselImages.scrollLeft % imageCarouselItemWidth === 0) {
-                // carouselImages.scrollLeft += imageCarouselItemWidth;
-                carouselImages.scrollBy({
-                    left: imageCarouselItemWidth,
-                    behavior: 'smooth'
-                });
-            } else {
-                // carouselImages.scrollLeft += imageCarouselItemWidth - carouselImages.scrollLeft % imageCarouselItemWidth;
-                carouselImages.scrollBy({
-                    left: imageCarouselItemWidth - carouselImages.scrollLeft % imageCarouselItemWidth,
-                    behavior: 'smooth'
-                });
             }
 
             // This is the logic for the nav indicator carousel
@@ -156,8 +211,25 @@ export default {
                 navCarouselItems[this.currentImageIndex-2].classList.add("edge");
             }
 
+            // If the user has scrolled so that we see two images, snap to the next image
+            if (carouselImages.scrollLeft % imageCarouselItemWidth === 0) {
+                // carouselImages.scrollLeft += imageCarouselItemWidth;
+                await this.smoothScrollBy(carouselImages, {
+                            left: imageCarouselItemWidth,
+                            duration: 400,
+                        });
+            } else {
+                await this.smoothScrollBy(carouselImages, {
+                    left: imageCarouselItemWidth - carouselImages.scrollLeft % imageCarouselItemWidth,
+                    duration: 400,
+                });
+            }
+
         },
         previous(e) {
+            this.handleButtonClick(e, this.performPrevScroll);
+        },
+        async performPrevScroll(e) {
                 let imageCarouselContainer;
                 imageCarouselContainer = e.target.closest('.image-carousel-container');
                     
@@ -190,19 +262,6 @@ export default {
                     this.currentImageIndex--;
                 }
 
-                // If the user has scrolled so that we see two images, snap to the previous image
-                if (carouselImages.scrollLeft % imageCarouselItemWidth === 0) {
-                    carouselImages.scrollBy({
-                        left: -imageCarouselItemWidth,
-                        behavior: 'smooth'
-                    });
-                } else {
-                    carouselImages.scrollBy({
-                        left: -carouselImages.scrollLeft % imageCarouselItemWidth,
-                        behavior: 'smooth'
-                    });
-                }
-
                 // This is the logic for the nav indicator carousel
                 navCarouselItems[this.currentImageIndex+1].classList.remove('active');
                 navCarouselItems[this.currentImageIndex].classList.remove("edge", "near-edge");
@@ -221,7 +280,25 @@ export default {
                     navCarouselItems[this.currentImageIndex+1].classList.add("near-edge");
                     navCarouselItems[this.currentImageIndex+2].classList.add("edge");
                 }
-        }
+
+                // If the user has scrolled so that we see two images, snap to the previous image
+                if (carouselImages.scrollLeft % imageCarouselItemWidth === 0) {
+                    await this.smoothScrollBy(carouselImages, {
+                            left: -imageCarouselItemWidth,
+                            duration: 400,
+                        });
+                    // carouselImages.scrollBy({
+                    //     left: -imageCarouselItemWidth,
+                    //     behavior: 'smooth'
+                    // });
+                } else {
+                    await this.smoothScrollBy(carouselImages, {
+                            left: -carouselImages.scrollLeft % imageCarouselItemWidth,
+                            duration: 400,
+                        });
+
+                }
+        },
     }
 }
 </script>
@@ -463,15 +540,26 @@ export default {
 
     /* Classes for the little dots at the bottom of the image carousel */
 
-    .nav-indicator-carousel-window {
-        display: block;
+    .wrapper-for-nav-indicator-carousel {
+        display: flex;
         position: absolute;
-        background-color: transparent;
+        justify-content: center;
+        align-content: center;
+        width: 100%;
+        height: 16px;
         bottom: 10px;
-        left: 35%;
+        /* overflow: hidden; */
+    }
+
+    .nav-indicator-carousel-window {
+        display: flex;
+        /* position: absolute; */
+        background-color: transparent;
+        /* bottom: 10px; */
+        /* left: 35%; */
         z-index: 1;
-        width: 30%;
-        height: 10%;
+        width: 90px;
+        /* height: 10%; */
         overflow-x: hidden;
     }
 
@@ -482,6 +570,7 @@ export default {
         align-items: center;
         height: 100%;
         transition: transform 0.2s ease-in-out;
+        background: transparent;
         /* width: 100%; */
     }
 
@@ -497,19 +586,22 @@ export default {
         margin-left: 5px;
         margin-right: 5px;
         box-sizing: border-box;
-        transition: background-color 0.2 ease;
+        /* transition: background-color 0.2 ease; */
     }
 
     .nav-indicator-carousel-item.active {
-        transform: scale(1.2);
+        border-radius: 50%;
+        transform: scale(1.1);
         background-color: rgb(255, 255, 255);
     }
 
     .nav-indicator-carousel-item.edge {
+        border-radius: 50%;
         transform: scale(0.7);
     }
 
     .nav-indicator-carousel-item.near-edge {
+        border-radius: 50%;
         transform: scale(0.85);
     }
 
